4aef2fa7a3faef8330a3193a349f8bcdd82fb659
Steve Lambe <steve@firrae.ca>
2021-02-07 13:53:19 -0500

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'home#index'
+
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
